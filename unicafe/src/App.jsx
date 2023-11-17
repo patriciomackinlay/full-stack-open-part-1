e76e84990d540bfad5980e9a933831cbdaf4ee0e
patriciomackinlay <141454324+patriciomackinlay@users.noreply.github.com>
2023-11-17 15:10:39 -0300

    Finished 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -12,13 +12,17 @@ const Display = ({rating, quantity}) => {
   )
 }
 
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positivePercentage = (good * 100) / all
 
   return (
     <div>
@@ -28,6 +32,9 @@ const App = () => {
       <Display rating={"good"} quantity={good} />
       <Display rating={"neutral"} quantity={neutral} />
       <Display rating={"bad"} quantity={bad} />
+      <Display rating={"all"} quantity={all} />
+      <Display rating={"average"} quantity={average} />
+      <Display rating={"positive"} quantity={positivePercentage + " %"} />
     </div>
   )
 }
